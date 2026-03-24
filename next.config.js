96c8981a758a89d8a343730d35cacf9d54c8f761
lazerd <darrinjco@gmail.com>
2026-03-24 15:16:05 -0700

    Add Next.js configuration for remote image patterns

---
+++ next.config.js
@@ -0,0 +1,17 @@
+js/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.getactive10.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
